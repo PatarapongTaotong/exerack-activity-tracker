4804e3e23ca63f6ab3fae0aa22cea0e88b995f63
Patarapong Taotong <t.patarapong@gmail.com>
2022-04-14 10:12:59 +0700

    Add sort box

--- src/Components/RecordResults/RecordResults.js
+++ src/Components/RecordResults/RecordResults.js
@@ -1,13 +1,26 @@
 import './RecordResults.css';
 import RecordList from '../RecordList/RecordList';
-import Button from '../Button/Button';
 
 const RecordResults = ({recordData, onClickRecord}) => {
     return (
         <div className="record-results">
             <div className="record-results-header">
                 <div className="record-title">Records</div>
-                <Button link="/activities">ADD</Button>
+                <div className="search-box">
+                    <p>Sort by</p>
+                    <select id="cars" name="cars">
+                        <option value="show all">Show all</option>
+                        <option value="run">Run</option>
+                        <option value="bicycle">Bicycle</option>
+                        <option value="ride">Ride</option>
+                        <option value="swim">Swim</option>
+                        <option value="walk">Walk</option>
+                        <option value="hike">Hike</option>
+                        <option value="weight-training">Weight-training</option>
+                        <option value="boxing">Boxing</option>
+                        <option value="yoga">Yoga</option>
+                    </select>
+                </div> 
             </div>
             <RecordList records={recordData} onClickRecord={onClickRecord} />
         </div>
