cf555fab94e37edab8fe167fd6d3ad23de6d81f2
Patarapong Taotong <t.patarapong@gmail.com>
2022-04-18 19:11:21 +0700

    Remove loader for Activities page

--- src/Pages/Activities/Activities.js
+++ src/Pages/Activities/Activities.js
@@ -1,21 +1,11 @@
 import './Activities.css';
-import { useState, useEffect } from 'react';
 import NavBar from '../../Components/NavBar/NavBar';
 import MainBoard from '../../Components/MainBoard/MainBoard';
 import ActivityList from '../../Components/ActivityList/ActivityList';
 import Footer from '../../Components/Footer/Footer';
 import QuickLink from '../../Components/QuickLink/QuickLink';
-import Loader from '../../Components/Loader/Loader';
 
 const Activities = () => {
-    const [showLoader, setShowLoader] = useState(false);
-
-    useEffect (() => {
-        setShowLoader(true);
-        setTimeout (() => {
-            setShowLoader(false);
-        }, 1000)
-    }, [])
 
     return (
         <>
@@ -24,7 +14,6 @@ const Activities = () => {
             <ActivityList />
             <QuickLink />
             <Footer>© EXERACK 2022</Footer>
-            {showLoader && <Loader />}
         </>
     );
 }
